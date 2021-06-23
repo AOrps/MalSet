7f34f2461e7eb46f2cb17c4d66838af5fb4b439a
AOrps <aeo34@njit.edu>
2021-06-23 16:57:48 -0400

    DB adds as well as structural changes to repo and improved functionality

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+# This file it just to have this directory be treated as a package
+
+#######################################################################
+
+
+
+# grab.py
+"""
+Description:
+
+"""
+
+
+
+# (catalogger).py
+"""
+Description:
+
+"""
